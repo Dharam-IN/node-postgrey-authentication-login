44f0517e73e147b269b82305db9f6bf2f9d344e9
dharamraj-dev <dharamrajyadav6312@gmail.com>
2023-10-28 13:17:33 +0530

    add database and package files

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE authentication_details;
+
+CREATE TABLE users (
+    user_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+);
+
+--insert dummy users
+
+INSERT INTO users(user_name, user_email, user_password) VALUES ('dummy', 'dummy@gmail.com', 'dpass541');
