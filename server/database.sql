4261bd402518810d26339dae64ea6333c7da4142
dharamraj-dev <dharamrajyadav6312@gmail.com>
2023-10-30 18:22:12 +0530

    user registration complete

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE authentication_details;
+CREATE DATABASE attendance;
 
-CREATE TABLE users (
+CREATE TABLE employees (
     user_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
@@ -10,3 +10,5 @@ CREATE TABLE users (
 --insert dummy users
 
 INSERT INTO users(user_name, user_email, user_password) VALUES ('dummy', 'dummy@gmail.com', 'dpass541');
+
+
--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const pool = require("../db")
+const bcrypt = require("bcrypt")
+const jwtGenerator = require('../utilis/jwtGenerator')
 
 module.exports = router;
 
@@ -15,19 +17,58 @@ router.post('/register', async(req, res)=>{
             email
         ]);
 
-        res.json(user.row)
+        // res.json(user.row)
+        if (user.rows.length > 0) {
+            return res.status(400).json({ error: "User with this email already exists" });
+        }
         
         
         // 3. Bcypt the user password
 
         const saltRound = 10
+        const salt = await bcrypt.genSalt(saltRound);
+
+        const bcryptpassword = await bcrypt.hash(password, salt);
 
         // 4. enter the new user inside the database
 
+        const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptpassword]);
+        // res.json(newUser.rows[0])
+
         // 5. genrating our jwt token
+
+        const token = jwtGenerator(newUser.rows[0].user_id);
+
+        // res.json({token})
+        res.json({
+            user: newUser.rows[0],
+            token: token, // Send the JWT token along with the user data
+          });
+          
 
     } catch (error) {
         console.log(error.message);
         res.status(500).send("Server Error")
     }
 })
+
+// login route 
+
+router.post("/login", async(req, res)=>{
+    try {
+        
+        // 1. destructure the req.body
+
+        // 2. check if user don't exist (if not then throw error)
+
+        // 3. check if incomming password is the same password database password
+
+        // 4. give them the jwt token
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Server Error")
+    }
+})
+
+module.exports = router
